e8517fc8ccaf2e781b14b683ad06da3efc708f84
Manuel Vio <manuelvio74@gmail.com>
2025-04-27 08:56:08 +0200

    Added License on unit kernal

--- units/kernal.pas
+++ units/kernal.pas
@@ -1,7 +1,38 @@
 unit kernal;
-{
-Info taken from https://sta.c64.org/cbm64krnfunc.html
-}
+(*
+
+MIT License
+
+Copyright (c) 2025 Manuel Vio
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+* @type: unit
+* @author: Manuel Vio <manuelvio74@gmail.com>
+* @name: Hardware reference library.
+* @version: 0.1
+
+* @description:
+* C64 Kernal functionso 
+* Inspired from equivalent Millfork library
+* Info from https://sta.c64.org/cbm64krnfunc.html
+*)
+
+
 interface
 
 type
@@ -35,31 +66,8 @@ uses stringUtils, sysutils;
 
 const
 
-(*
-SETNAM. Set file name parameters.
-Input: A = File name length; X/Y = Pointer to file name.
-Output: –
-Used registers: –
-Real address: $FDF9.
-*)
 KERNAL_SETNAM      = $FFBD;
-
-(*
-SETLFS. Set file parameters.
-Input: A = Logical number; X = Device number; Y = Secondary address.
-Output: –
-Used registers: –
-Real address: $FE00.
-*)
 KERNAL_SETLFS      = $FFBA;
-
-(*
-OPEN. Open file. (Must call SETLFS and SETNAM beforehands.)
-Input: –
-Output: –
-Used registers: A, X, Y.
-Real address: ($031A), $F34A.
-*)
 KERNAL_OPEN        = $FFC0;
 KERNAL_CLOSE       = $FFC3;
 KERNAL_CLRCHN      = $FFCC;
